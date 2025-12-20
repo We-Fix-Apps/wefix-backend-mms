16224efa286ef32eb79f9b56064485373b86196f
Jadaboawwad <jadaboawwad@gmail.com>
2025-12-20 13:28:57 +0300

    feat : add ticket feature

--- src/RESTful/routes/ticket.routes.ts
+++ src/RESTful/routes/ticket.routes.ts
@@ -8,6 +8,8 @@ const router = express.Router();
 router.get('/', authenticateToken, ticketController.getCompanyTickets);
 router.get('/statistics', authenticateToken, ticketController.getTicketStatistics);
 router.get('/:id', authenticateToken, ticketController.getTicketById);
+router.post('/', authenticateToken, ticketController.createTicket);
+router.put('/:id', authenticateToken, ticketController.updateTicket);
 
 export default router;
 
